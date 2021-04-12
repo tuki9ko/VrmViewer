f830cbe4b2c8336cf92b2b554218256967c4c93c
tuki9ko <arkmisha@gmail.com>
2021-04-12 10:13:57 +0900

    infrastructureのファイルを整理

--- src/infrastructure/repository/ModelInfoRepository.ts
+++ src/infrastructure/repository/ModelInfoRepository.ts
@@ -1,5 +1,5 @@
 import { VRM } from "@pixiv/three-vrm";
-import { PromiseGLTFLoader } from "./PromiseGLTFLoader";
+import { PromiseGLTFLoader } from "./../loader/PromiseGLTFLoader";
 
 import { Postgres } from "./Postgres"
 import { ModelInfo } from "../../domain/model/ModelInfo";
